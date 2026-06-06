64abcedb4cbb3a79fb8ed8f679c0539cd4b8ccc1
Sean Collins <cllns@users.noreply.github.com>
2026-06-06 16:37:31 -0600

    Reinstate inheritance integration spec (#509)
    * Reinstate inheritance spec
    
    'resources' used to be available in router, now it's in hanami/hanami
    
    So this is a suitable replacement
    
    * Move Inheritance module and classes to spec
    
    * Use anonymous classes in inheritance spec
    
    Named constants leak for the lifetime of the test suite, since
    anonymous classes define no constants and are truly isolated.

--- spec/integration/inheritance_spec.rb
+++ spec/integration/inheritance_spec.rb
@@ -6,10 +6,72 @@ RSpec.describe Hanami::Action do
   describe "inheritance" do
     include Rack::Test::Methods
 
-    let(:app) { Inheritance::Application.new }
+    let(:base_action) do
+      Class.new(Hanami::Action) do
+        before :log_base_action
 
-    before do
-      pending
+        private
+
+        def log_base_action(*, res)
+          res[:base_action] = true
+        end
+      end
+    end
+
+    let(:authenticated_action) do
+      Class.new(base_action) do
+        before :authenticate!
+
+        private
+
+        def authenticate!(*, res)
+          res[:authenticated] = true
+        end
+      end
+    end
+
+    let(:restful_action) do
+      Class.new(authenticated_action) do
+        before :find_book
+        after :render
+
+        private
+
+        def find_book(req, res)
+          res[:book] = "book #{req.params[:id]}"
+        end
+
+        def render(*, res)
+          res.body = res.exposures.keys
+        end
+      end
+    end
+
+    let(:show_action) do
+      Class.new(restful_action) do
+        def handle(*, res)
+          res[:found] = true
+        end
+      end
+    end
+
+    let(:destroy_action) do
+      Class.new(show_action) do
+        def handle(*, res)
+          super
+          res[:destroyed] = true
+        end
+      end
+    end
+
+    let(:app) do
+      show    = show_action.new
+      destroy = destroy_action.new
+
+      Hanami::Router.new do
+        get    "/books/:id", to: show
+        delete "/books/:id", to: destroy
+      end
     end
 
     it "calls the exact chain of events" do
