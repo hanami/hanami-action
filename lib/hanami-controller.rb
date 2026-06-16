5eb99a5f1d932b640da7ccbf33c63844c63f2b08
Tim Riley <tim@riley.id.au>
2026-06-16 13:32:06 +1000

    Drop the controller require files
    We decided to do a hard cutoff in the hanami-controller -> hanami-action gem naming transition. These files existed to serve a longer deprecation process, so we don’t need them anymore.

--- lib/hanami-controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "hanami/action"
-
-warn(
-  "`require \"hanami-controller\"` is deprecated and will be removed in Hanami 3.1. " \
-  "Use `require \"hanami-action\"` instead.",
-  category: :deprecated
-)
