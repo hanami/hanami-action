db269898a663053db087f9ff4e82a27fd4235870
Ziggy Gable <ziggy@ziggythehamster.sh>
2026-08-13 17:41:14 -0700

    Add a spec that after actions support non-proc callables

--- spec/unit/hanami/action/after_spec.rb
+++ spec/unit/hanami/action/after_spec.rb
@@ -18,5 +18,13 @@ RSpec.describe Hanami::Action do
       expect(response[:egg]).to       eq("Coque".reverse)
       expect(response[:arguments]).to eq(["Hanami::Action::Request", "Hanami::Action::Response"])
     end
+
+    it "invokes the given callable object after the action is run" do
+      action = AfterCallableAction.new
+      response = action.call({})
+
+      expect(response[:egg]).to   eq("Ei!".reverse)
+      expect(response[:arguments]).to eq(["Hanami::Action::Request", "Hanami::Action::Response"])
+    end
   end
 end
